19efa45ee58a5502f38617a1e9609645ded952d1
ravikanojiya <ravi.kanojiya@aitron.in>
2026-07-08 10:30:17 +0530

    feat: wire router and auth into app bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+import { useAuth } from './composables/useAuth'
 import './style.css'
 
-createApp(App).mount('#app')
+const { initAuth } = useAuth()
+
+initAuth().finally(() => {
+  createApp(App).use(router).mount('#app')
+})
